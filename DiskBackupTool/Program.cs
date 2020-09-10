9ec7a9612b6f2dcf1df9ff91a687a49de1168372
olly <olly.george@gmail.com>
2020-09-10 16:12:43 +0100

    Initial commit

---
+++ DiskBackupTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiskBackupTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
